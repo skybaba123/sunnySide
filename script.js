c5bd453cfc7c04a2b8b201e90a38fdd15a96ea6c
Ugochukwu Miracle <77087947+skybaba123@users.noreply.github.com>
2021-08-09 14:49:18 +0100

    ADDED FULL FILE

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector(".menu");
+const menuBar = document.querySelector(".menu-bar");
+const moda = document.querySelector(".moda");
+const moda1 = document.querySelector(".moda1");
+
+menu.addEventListener("click", function () {
+  menuBar.classList.toggle("menu-bar1");
+});
